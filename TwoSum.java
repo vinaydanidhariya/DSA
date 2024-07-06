89b09541c8fa0a40b6e876ff40c7f67f431219b5
vinay danidhariya <vinaydanidhariya4114@gmail.com>
2024-07-06 09:43:36 +0530

    "01 FIRST PROBLEM"

---
+++ TwoSum.java
@@ -0,0 +1,58 @@
+// 1. Two Sum
+// Easy
+// Topics
+// Companies
+// Hint
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+
+// Input: nums = [3,2,4], target = 6
+// Output: [1,2]
+// Example 3:
+
+// Input: nums = [3,3], target = 6
+// Output: [0,1]
+
+// Constraints:
+
+// 2 <= nums.length <= 104
+// -109 <= nums[i] <= 109
+// -109 <= target <= 109
+// Only one valid answer exists.
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            int diff = target - nums[i];
+            if (map.containsKey(diff)) {
+                return new int[] { map.get(diff), i };
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
+
+class TwoSum {
+    public static void main(String[] args) {
+        int arr[] = { 3, 2, 4 };
+        int target = 6;
+
+        Solution s1 = new Solution();
+        int newarr[] = s1.twoSum(arr, target);
+        System.out.println("index are" + newarr[0] + " " + newarr[1]);
+    }
+}
